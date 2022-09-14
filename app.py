a7686c9111700bba2f3f9017e31c57711cf27311
Jayanthi C <96628596+Jayanthi061993@users.noreply.github.com>
2022-09-14 11:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 
-app = Flask(__name__,template_folder = r"/") 
+app = Flask(__name__)
                                                                                
     
 clf = pickle.load(open('model.pkl','rb'))
